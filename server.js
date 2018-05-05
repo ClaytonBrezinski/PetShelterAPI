dd8221035f47b1ef350862edac5c17832fcc37ae
Unknown <clayton.brez@gmail.com>
2018-05-05 17:55:21 -0600

    add body-parser to project
    allows POSTs to get to express and be worked with

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require("express");
 const path = require('path');
 const PORT = process.env.PORT || 3000
 const app = express();
+var bodyParser = require('body-parser');
 var db = require('./databases/queries.js');
 
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .use(bodyParser.json())
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.send("hello world"))
   // route handlers for server's REST API
   .get('/api/pets', db.getAllPets)
   .get('/api/pets/:id', db.getPetById)
-  // .post('/api/puppies', db.createPet)
+  .post('/api/pets', db.createPet)
   .listen(PORT, () => console.log(`Listening on ${ PORT }`)
 )
 
