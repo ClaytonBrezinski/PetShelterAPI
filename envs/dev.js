be60090e665de2afc444f43c44017f6766ae21fe
Unknown <clayton.brez@gmail.com>
2018-05-05 16:12:34 -0600

    add env variables for the project

---
+++ envs/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: '',
+    port: 0,
+    database: '',
+    user: '',
+    password: ''
+}
---
+++ envs/prod.js
@@ -0,0 +1,7 @@
+module.exports = {
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_URL,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+};
