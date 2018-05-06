2d92ada50f4a437af7fa72a2d46ea3c1f4260d78
Unknown <clayton.brez@gmail.com>
2018-05-05 18:46:25 -0600

    refactor server.js to better code standard

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
 const express = require("express");
-const path = require('path');
-const PORT = process.env.PORT || 3000
-const app = express();
-var bodyParser = require('body-parser');
-var db = require('./databases/queries.js');
+const path = require("path");
+const PORT = process.env.PORT || 3000;
 
+var bodyParser = require("body-parser");
+var db = require("./databases/queries.js");
+var app = express();
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .use(bodyParser.json())
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.send("hello world"))
-  // route handlers for server's REST API
-  .get('/api/pets', db.getAllPets)
-  .get('/api/pets/:id', db.getPetById)
-  .post('/api/pets', db.createPet)
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`)
-)
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.get("/", (req, res) => res.send("hello world"));
+// route handlers for server's REST API
+app.get("/api/pets", db.getAllPets);
+app.get("/api/pets/:id", db.getPetById);
+app.post("/api/pets", db.createPet);
 
-
-
+// DEV error handler - print out the stack trace
+// PROD error handler - remove stack traces
+app.get("env") === "development"
+    ? app.use((err, req, res, next) => {
+          res.status(err.code || 500).json({
+              status: "error",
+              message: err,
+          });
+      })
+    : app.use((err, req, res, next) => {
+          res.status(err.status || 500).json({
+              status: "error",
+              message: err.message,
+          });
+      });
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
